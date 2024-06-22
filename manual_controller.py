53f24df47de040ddbcbb30b25b467133f8c8201e
AndreGutierrezTecMx <114358720+AndreGutierrezTecMx@users.noreply.github.com>
2024-06-22 17:41:53 -0600

    Main files and worl updates

--- manual_controller.py
+++ manual_controller.py
@@ -4,19 +4,33 @@ from controller import Robot, Camera, Display, Keyboard
 from vehicle import Car, Driver
 import numpy as np
 from PIL import Image
-import cv2
-import tensorflow as tf
+
+# Crear el directorio para almacenar las imágenes y el archivo CSV si no existen
+output_dir = "Train_data_navegacion_autonoma"
+images_dir = os.path.join(output_dir, "images")
+os.makedirs(images_dir, exist_ok=True)
+csv_file = os.path.join(output_dir, "data.csv")
+
+# Inicializar el archivo CSV
+if not os.path.exists(csv_file):
+    with open(csv_file, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["path_img_right", "path_img_left", "path_img_front", "angle", "steering_angle"])
 
 # Función para obtener dispositivos
-def get_device(robot, type_of_device):
+def get_devices(robot, type_of_device):
     if type_of_device == "camera" or type_of_device == "display":
+        device_right = robot.getDevice(f"right_{type_of_device}")
+        device_left = robot.getDevice(f"left_{type_of_device}")
         device_front = robot.getDevice(f"front_{type_of_device}")
         if type_of_device == "display":
-            return device_front
+            return device_right, device_left, device_front
     timestep = int(robot.getBasicTimeStep())
     if type_of_device == "camera":
+        device_right.enable(timestep)
+        device_left.enable(timestep)
         device_front.enable(timestep)
-        return device_front
+        return device_right, device_left, device_front
     keyboard = Keyboard()
     keyboard.enable(timestep)
     return keyboard    
@@ -33,15 +47,27 @@ def display_camera_image(camera, display):
     display.imageDelete(displayed_image)
     return rgb_image  # Devolver la imagen RGB para guardarla
 
+# Función para guardar imágenes y registrar en el CSV
+def save_images_and_log(rgb_images, angle, steering_angle, img_num):
+    right_img_path = os.path.join(images_dir, f"right_image_{img_num}.png")
+    left_img_path = os.path.join(images_dir, f"left_image_{img_num}.png")
+    front_img_path = os.path.join(images_dir, f"front_image_{img_num}.png")
+    
+    Image.fromarray(rgb_images[0]).save(right_img_path)
+    Image.fromarray(rgb_images[1]).save(left_img_path)
+    Image.fromarray(rgb_images[2]).save(front_img_path)
+
+    with open(csv_file, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([right_img_path, left_img_path, front_img_path, angle, steering_angle])
+
 # Inicialización de ángulos y velocidad
 manual_steering = 0
 steering_angle = 0
 angle = 0.0
-speed = 20
-# Ruta donde se encuentra el modelo guardado
-modelo_ruta = 'nvidia_model_5.keras'
-# Cargar el modelo
-modelo = tf.keras.models.load_model('nvidia_model.keras')
+speed = 15
+img_num = 0  # Contador de imágenes
+recording = False
 
 # Establecer la velocidad objetivo
 def set_speed(kmh):
@@ -75,34 +101,20 @@ def change_steer_angle(inc):
         turn = "left" if steering_angle < 0 else "right"
         print("turning {} rad {}".format(str(steering_angle), turn))
 
-def img_preprocess(img):
-  # Recortar la parte superior y lateral de la imagen
-  img_crop = img[35:60, 0:60]
-  # Escalar la imagen a 200x66
-  img_crop = cv2.resize(img_crop, (200, 66))
-  # Transformar el color
-  img_crop = cv2.cvtColor(img_crop, cv2.COLOR_RGB2YUV)
-  # Aplicar desenfoque Gaussiano
-  img_crop = cv2.GaussianBlur(img_crop, (3, 3), 0)
-  return np.expand_dims(img_crop, axis=0) 
-
-
 # Función principal
 def main():
-    global img_num, recording
+    global img_num, recording, steering_angle
     robot = Car()
     driver = Driver()
     
-    camera_front = get_device(robot, "camera")
-    display_front = get_device(robot, "display")
-    keyboard = get_device(robot, "keyboard")
+    camera_right, camera_left, camera_front = get_devices(robot, "camera")
+    display_right, display_left, display_front = get_devices(robot, "display")
+    keyboard = get_devices(robot, "keyboard")
     
     while robot.step() != -1:
+        rgb_right = display_camera_image(camera_right, display_right)
+        rgb_left = display_camera_image(camera_left, display_left)
         rgb_front = display_camera_image(camera_front, display_front)
-        rgb_front = img_preprocess(rgb_front)
-        angle_front = modelo.predict(rgb_front)
-        mean_angle = angle_front
-        print(f"The mean angle is: {mean_angle}")
 
         key = keyboard.getKey()
         if key == Keyboard.UP:
@@ -111,8 +123,23 @@ def main():
         elif key == Keyboard.DOWN:
             set_speed(speed - 5.0)
             print("down")
-        driver.setSteeringAngle(mean_angle)
+        elif key == Keyboard.RIGHT:
+            change_steer_angle(+0.8)
+            print("right")
+        elif key == Keyboard.LEFT:
+            change_steer_angle(-0.8)
+            print("left")
+        elif key == ord('R'):  # Empezar a capturar el video
+            recording = True
+            print("Start recording")
+        elif key == ord('S'): # Terminar de capturar el video
+            recording = False
+            print("Stop recording")
+        driver.setSteeringAngle(angle)
         driver.setCruisingSpeed(speed)
+        if (recording): 
+            save_images_and_log([rgb_right, rgb_left, rgb_front], angle, steering_angle,  img_num)
+            img_num += 1
 
 if __name__ == "__main__":
     main()
